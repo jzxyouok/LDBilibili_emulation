d1b71446c8acf64efbf6bbb229f32c3576f009c8
xlDon <805898144@qq.com>
2017-05-25 23:08:48 +0800

    提交代码

---
+++ LDBilibili_emulation/ThirdPartyLibrary/ZPSegmentBar/UIColor+Extension.swift
@@ -0,0 +1,31 @@
+//
+//  UIColor+Extension.swift
+//  ZPSegmentBar
+//
+//  Created by xlDon on 17/3/9.
+//  Copyright © 2017年 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor
+{
+   public func getRGBValue() -> (CGFloat ,CGFloat,CGFloat) {
+        
+
+    
+        var red : CGFloat = 0
+        var green : CGFloat = 0
+        var blue : CGFloat = 0
+        getRed(&red, green: &green, blue: &blue, alpha: nil)
+        
+//        return (red * 255, green * 255, blue * 255)
+    return (red , green , blue )
+    
+    }
+    
+   public class func randomColor() -> UIColor
+    {
+        return UIColor(red: CGFloat(arc4random_uniform(256))/255.0, green: CGFloat(arc4random_uniform(256))/255.0, blue: CGFloat(arc4random_uniform(256))/255.0, alpha: 1.0)
+    }
+}
