5fee6555353d0a3670ca7ca8e0cef8f7fb01c03c
xlDon <805898144@qq.com>
2017-05-27 23:29:30 +0800

    提交代码

--- LDBilibili_emulation/Classes/Homepage/Controller/HomepageViewController.swift
+++ LDBilibili_emulation/Classes/Homepage/Controller/HomepageViewController.swift
@@ -12,6 +12,17 @@ class HomepageViewController: UIViewController {
     
     override func viewDidLoad() {
         setupChildViewControllers()
+        
+        var style = ZPStyle()
+        style.isScrollEnabled = true; //标题是否可以滚动,默认为true;
+        style.isShowCover = false    //标题是否显示遮盖,默认为true;
+        style.isShowBottomLine = true //标题下方是否显示BottomLine,默认为true;
+        style.isNeedScale = true      //标题文字是否缩放,默认为true;
+        
+        let frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
+        let segmentView = ZPSegmentBarView(frame: frame, titles: titles, style: style, childVcs: childVcs, parentVc: self)
+        view.addSubview(segmentView)
+        
         super.viewDidLoad()
     }
     
@@ -44,3 +55,27 @@ class HomepageViewController: UIViewController {
         addChildViewController(bVC)
     }
 }
+
+extension HomepageViewController : ZPPageBarViewDataSource
+{
+    func numberOfSections(in pageBarView: ZPPageBarView) -> Int {
+        return 3
+    }
+    
+    func pageBarView(_ pageBarView: ZPPageBarView, numberOfItemsInSection section: Int) -> Int {
+        if section == 0 {
+            return 61
+        } else if section == 1 {
+            return 18
+        } else if section == 2 {
+            return 40
+        }
+    }
+    
+    func pageBarView(_ pageBarView: ZPPageBarView, collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kCollectionViewCellID, for: indexPath)
+        cell.backgroundColor = UIColor.randomColor()
+        return cell
+    }
+}
+
