a5b7da1793f087972f474805df9429ac4442fc92
xlDon <805898144@qq.com>
2017-06-21 23:23:13 +0800

    提交代码

---
+++ LDBilibili_emulation/LDKit/YPBilibiliNormalRefresh.h
@@ -0,0 +1,13 @@
+//
+//  YPBilibiliNormalRefresh.h
+//  LDBilibili_emulation
+//
+//  Created by xlDon on 2017/6/21.
+//  Copyright © 2017年 xlDon. All rights reserved.
+//
+
+#import <MJRefresh/MJRefresh.h>
+
+@interface YPBilibiliNormalRefresh : MJRefreshHeader
+
+@end
